077910a9936c74162569d5451cec884b4a13f4af
Preeti Kulkarni <kulkarni.pre@northeastern.edu>
2024-02-29 13:22:13 -0500

    added metadata start up scripts for sql

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("./google-cred.json")
+  credentials = file(var.googlecred)
   project     = var.project_id
   region      = var.region
 }
@@ -16,6 +16,7 @@ resource "google_compute_subnetwork" "webapp_subnet" {
   ip_cidr_range = var.webapp-cidr
   region        = var.region
   network       = google_compute_network.vpc.self_link
+  private_ip_google_access = true
 }
 
 resource "google_compute_subnetwork" "db_subnet" {
@@ -50,10 +51,10 @@ resource "google_compute_firewall" "allow" {
 
   allow {
     protocol = "tcp"
-    ports    = [var.application_port]
+    ports    = [var.application_port, "5432"]
   }
   target_tags = ["http-server"]
-  source_ranges = ["0.0.0.0/0"]
+  source_ranges = ["0.0.0.0/0", google_compute_subnetwork.webapp_subnet.ip_cidr_range]
   
 }
 
@@ -68,6 +69,59 @@ resource "google_compute_firewall" "deny" {
   source_ranges = ["0.0.0.0/0"]
 }
 
+# Private IP Configuration
+resource "google_compute_global_address" "private_ip_address" {
+  name          = var.ip_name 
+  purpose       = var.ip_purpose
+  address_type  = var.ip_type
+  prefix_length = 16
+  network       = google_compute_network.vpc.self_link 
+}
+
+# Google Service Networking Connection
+resource "google_service_networking_connection" "default" {
+  network                 = google_compute_network.vpc.self_link
+  service                 = var.service_name 
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+}
+
+resource "google_sql_database_instance" "db_instance" {
+  name             = var.db_instance_name
+  project          = var.project_id
+  region           = var.region
+
+  deletion_protection = false
+  depends_on = [google_service_networking_connection.default]
+  database_version    = var.database_version
+  settings {
+    tier = var.db_tier
+    disk_type = var.db_disk_type
+    disk_size = var.db_size
+    availability_type = var.db_availablity_type
+    ip_configuration {
+      ipv4_enabled = false
+      private_network = google_compute_network.vpc.id
+    }
+  }
+}
+
+resource "google_sql_database" "webapp_db" {
+  name     = var.sql_db_name
+  instance = google_sql_database_instance.db_instance.name
+}
+
+resource "random_password" "db_password" {
+  length           = 16
+  special          = true
+  override_special = var.override_special
+}
+
+resource "google_sql_user" "webapp_user" {
+  name     = var.sql_user_name
+  instance = google_sql_database_instance.db_instance.name
+  password = random_password.db_password.result
+  depends_on = [google_sql_database.webapp_db]
+}
 
 resource "google_compute_instance" "vm-instance" {
   boot_disk {
@@ -87,6 +141,18 @@ resource "google_compute_instance" "vm-instance" {
     subnetwork  = google_compute_subnetwork.webapp_subnet.self_link
   }
   zone = "us-west1-b"
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    set -e
+
+    env_file="/opt/webapp/.env"
+    echo "DB_HOST=${google_sql_database_instance.db_instance.ip_address.0.ip_address}" > "$env_file"
+    echo "DB_PORT=${var.postgres_port}" >> "$env_file"
+    echo "DB_USER=${google_sql_user.webapp_user.name}" >> "$env_file"
+    echo "DB_NAME=${google_sql_database.webapp_db.name}" >> "$env_file"
+    echo "DB_PASSWORD=${random_password.db_password.result}" >> "$env_file"
+    echo "DB_DIALECT=${var.postgres_dialect}" >> "$env_file"
+  EOT
   tags = ["http-server"]
   depends_on = [ google_compute_firewall.allow, google_compute_subnetwork.webapp_subnet ]
 }
